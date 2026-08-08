92f928f80304ad32b2d59c6565a47a4fe6b50674
Kishore <pithanikishore@gmail.com>
2026-08-08 11:54:50 +0530

    Added Gunicorn server

--- App_1/views.py
+++ App_1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(self):
-    return HttpResponse("Hello These is basic page that is using for docker deployment learning")
+    return HttpResponse("Hello These is basic page that is using for docker deployment learning. Addedd Gunirconr server step")
--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "docker_deployment.wsgi:application"]
